5fd4bb9a79c318d6e554cc9174a8b057167331ef
bttmndl <bttmndl@gmail.com>
2023-10-16 09:13:02 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -45,12 +45,26 @@ const App = () => {
   }, []);
   console.log(SVG_SIZE);
   return (
-    <div className="App">
-      {[...Array(34)].map((e,i)=>(i%2===0 && i>= 4 && <button onClick={()=>setCount(i)}><h1>{i}</h1></button>))}
-      {/* <Header /> */}
-      {/* <Body /> */}
-      {/* <Socket /> */}
-      <LudoBoard playerCount={count} SVG_SIZE={SVG_SIZE} />
+    <div
+      className="App"
+      style={{ display: "flex", justifyContent: "space-around" }}
+    >
+      <div>
+        {[...Array(34)].map(
+          (e, i) =>
+            i % 2 === 0 &&
+            i >= 4 && (
+              <button onClick={() => setCount(i)}>
+                <h1>{i}</h1>
+              </button>
+            )
+        )}
+
+        {/* <Header /> */}
+        {/* <Body /> */}
+        {/* <Socket /> */}
+        <LudoBoard playerCount={count} SVG_SIZE={SVG_SIZE} />
+      </div>
       {/* <ThreeD /> */}
       <Main />
     </div>
