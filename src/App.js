d6bec95bf389c39578ac34a588ac912ae26eb191
bttmndl <bttmndl@gmail.com>
2023-11-20 19:42:07 +0530

    cleanup3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Main from "./Main";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import LudoBoard from "./LudoBoard";
 
 
@@ -10,9 +10,9 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* <div>
+      <div>
         <Header />
-      </div> */}
+      </div>
       <div style={{ display: "flex", justifyContent: "space-around" }}>
         <LudoBoard playerCount={count} SVG_SIZE={SVG_SIZE} />
         <Main />
